d448ebb443855336283bd7724ebae4ff70ac213b
Kevin Chau <kcheez@Kevins-MacBook-Pro.local>
2018-09-21 12:23:38 -0700

    changed id to render to

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -43,5 +43,5 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('zagat-info'));
 // window.Info = App;
